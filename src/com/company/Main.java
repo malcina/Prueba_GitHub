b76e5900deb9c51dc454c7bb4b5ed47151ee6c0a
malcina <malcina@gmail.com>
2022-03-24 17:34:55 +0100

    Segundo commit del proyecto ejem_GitHub

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!!");
         System.out.println("Hello again!!");
-        System.out.println("Primera refactorización.");
+        System.out.println("Primera refactorización modificada.");
+        System.out.println("Hacemos modificaciones para hacer otro PULL.");
+        System.out.println("Pruebo a hacer un PULL sin hacer commit antes");
     }
 }
