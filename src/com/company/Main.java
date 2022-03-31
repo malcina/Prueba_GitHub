e3c1064233cd5f4fc0935ff433bee54ad058bb8a
malcina <malcina@gmail.com>
2022-03-31 12:53:44 +0200

    Tercer commit del proyecto ejem_GitHub, modificando el fichero gitignore

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,5 +8,6 @@ public class Main {
         System.out.println("Primera refactorización modificada.");
         System.out.println("Hacemos modificaciones para hacer otro PULL.");
         System.out.println("Pruebo a hacer un PULL sin hacer commit antes");
+        System.out.println("Hago cambios en el código, después del PULL. Otro cambio más para probar el commit and push");
     }
 }
